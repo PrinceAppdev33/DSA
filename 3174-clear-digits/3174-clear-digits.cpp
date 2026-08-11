0c7b1f0f7f762863635cddcd1965ed96242a7c1b
PrinceAppdev33 <p87614073@gmail.com>
2026-08-11 11:46:23 +0530

    Time: 0 ms (100%), Space: 9.1 MB (34.7%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char>st;
+        for(char c : s){
+            if(!isdigit(c)) st.push(c);
+            else {
+                if(!st.empty()) st.pop();
+            }
+        }
+        string ans;
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
