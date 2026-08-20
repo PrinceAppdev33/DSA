617a548b555e322f431189ba1c5a2c7e5fd49abb
PrinceAppdev33 <p87614073@gmail.com>
2026-08-20 14:32:46 +0530

    Time: 0 ms (100%), Space: 35.4 MB (64.4%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -11,15 +11,12 @@
  */
 class Solution {
 public:
-    TreeNode* solve(TreeNode* root, int val){
+    TreeNode* searchBST(TreeNode* root, int val) {
         if(root == NULL) return NULL;
         if(root->val == val) return root;
 
-        if(val < root->val) return solve(root->left,val);
-        if(val > root->val) return solve(root->right,val);
+        if(val < root->val) return searchBST(root->left,val);
+        if(val > root->val) return searchBST(root->right,val);
         return NULL;
-    }
-    TreeNode* searchBST(TreeNode* root, int val) {
-        return solve(root,val);
     }
 };
