2085c28ade81bf0dfaac559e2aac328dad3aefae
PrinceAppdev33 <p87614073@gmail.com>
2026-09-05 15:13:01 +0530

    Time: 3 ms (71.43%), Space: 17.8 MB (93.61%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findOrder(int v, vector<vector<int>>& a) {
+        // code here
+        vector<int>adj[v+1];
+        for(int i=0;i<a.size();i++){
+            adj[a[i][1]].push_back(a[i][0]);
+        }
+        vector<int>indegree(v,0);
+        for(int i=0;i<v;i++){
+            for(auto it : adj[i]) indegree[it]++;
+        }
+        queue<int>q;
+        for(int i=0;i<v;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        if(topo.size()==v)return topo;
+        return {};
+    }
+};
